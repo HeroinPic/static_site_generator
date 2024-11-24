57480d10e35a082439477328cdb5d71f83e306f6
piccinimarco96@gmail.com <piccinimarco96@gmail.com>
2024-11-24 09:56:59 +0000

    add new method

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 from textnode import *
 from htmlnode import *
+from inline_markdown import *
+
 
 
 def main():
-    text_node_one = TextNode("slurp", TextType.BOLD, "https//:www.boot.dev")
-    text_node_one.__repr__()
-    html_node_one = HTMLNode("slurped", "TextType.BOLD", "https//:www.boot.dev",)
-    html_node_two = HTMLNode()
-    html_node_two.__repr__()
-    node7 = TextNode("This is a text node", TextType.TEXT, None)
-    print (node7.text_node_to_html_node())
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    print (extract_markdown_images(text))
 
 main()
